c8354292e68207d4e6aaba862a5fa5b909a94683
pangkajpaulclcare <pangkaj@cloudly.io>
2019-10-15 14:05:23 +0600

    Add Pytest
    - rename function names (e.g. prefix test_)
    - Add Pipfile and Pipfile.lock

--- tests/test_issuers.py
+++ tests/test_issuers.py
@@ -4,13 +4,13 @@ from card_validator.issuers import get_issuer
 # Why is Pytest not recognizing these tests?
 
 
-def visa_test():
+def test_visa():
     assert get_issuer('4862876677853409') == 'Visa'
 
 
-def mastercard_test():
+def test_mastercard():
     assert get_issuer('5462876677853409') == 'MasterCard'
 
 
-def american_express_test():
+def test_american_express():
     assert get_issuer('3462876677853409') == 'American Express'
